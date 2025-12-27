f225a53d49be001fd871d0428488b5e2288a4eb6
beomjo <dongjun1315@naver.com>
2025-12-27 22:49:48 +0900

    feat(signup.html): 회원가입 페이지 구현 완료

--- src/main/java/com/example/cmcboard/presentation/controller/UserController.java
+++ src/main/java/com/example/cmcboard/presentation/controller/UserController.java
@@ -9,7 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/api/user")
@@ -23,7 +22,7 @@ public class UserController {
     }
 
     @PostMapping("/signup")
-    public ResponseEntity<Long> signup(@RequestBody UserToEntity dto) {
+    public ResponseEntity<Long> signup(UserToEntity dto) {
         try {
             Long userId = userService.signUp(dto);
             return ResponseEntity.ok(userId);
