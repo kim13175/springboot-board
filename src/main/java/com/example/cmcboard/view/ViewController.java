5a603f8af68ae496e7e373df15ffb39643047cf7
beomjo <dongjun1315@naver.com>
2025-12-27 12:08:00 +0900

    feat(view/controller): 페이지 반환하는 view 컨트롤러 구현

--- src/main/java/com/example/cmcboard/view/ViewController.java
+++ src/main/java/com/example/cmcboard/view/ViewController.java
@@ -1,2 +1,24 @@
-package com.example.cmcboard.view;public class ViewController {
+package com.example.cmcboard.view;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class ViewController {
+    @GetMapping("/login")
+    public String moveLoginPage() {
+        return "login";
+    }
+
+    @GetMapping("/sign-up")
+    public String moveSignupPage() {
+        return "signup";
+    }
+
+    @GetMapping("/post")
+    public String movePostPage() {
+        return "post";
+    }
 }
